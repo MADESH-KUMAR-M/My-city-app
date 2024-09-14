0cc46c9521e767ab0d93f77c5e586cc23a4bc208
madesh-kumar-m <madeshkumar.vnb2006@gmail.com>
2024-09-14 22:56:13 +0530

    removed default

--- app/src/main/java/com/example/mycity/MainActivity.kt
+++ app/src/main/java/com/example/mycity/MainActivity.kt
@@ -20,28 +20,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyCityTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyCityTheme {
-        Greeting("Android")
-    }
-}
